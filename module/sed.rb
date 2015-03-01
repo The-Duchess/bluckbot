c33e6b598a731c02b0d1038029762dd5ebce8659
apels <iarcher@pdx.edu>
2015-03-01 00:57:20 -0800

    Update sed.rb

--- module/sed.rb
+++ module/sed.rb
@@ -51,7 +51,8 @@ class PLUGIN < Pluginf
 		if included then
 		#perform sed search replace on a
 			system("echo \"#{string_a}\" > temp")
-			@r.concat(`sed -e \"#{command_s}\" < temp`)
+			@r_s = `sed -e \"#{command_s}\" < temp`
+			@r.concat(@r_s.to_s)
 			#@r.concat(string_a.sub(sed_a, sed_b)) #this works as well but does not offer some options
 			system("rm -f temp")
 		else
