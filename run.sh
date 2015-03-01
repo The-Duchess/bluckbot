e4e0c43c8f9dc0c77a216488ae4d40d3ddc7ce48
apels <iarcher@pdx.edu>
2015-03-01 00:32:39 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ TRUE_T="TRUE"
 FALSE_T="FALSE"
 . ./res/.config.sh
 if [ $PASS_YN == $TRUE_T ]; then
-	ruby-2.1 ircboats.rb "$NETWORK_N" "$PORT_V" "$CHANNEL_N" "$LOGGING_YN" "PASS" "$PASSPHRASE"
+	ruby ircboats.rb "$NETWORK_N" "$PORT_V" "$CHANNEL_N" "$LOGGING_YN" "PASS" "$PASSPHRASE"
 else
 	if [ $PASS_YN == $FALSE_T ]; then
-		ruby-2.1 ircboats.rb "$NETWORK_N" "$PORT_V" "$CHANNEL_N" "$LOGGING_YN"
+		ruby ircboats.rb "$NETWORK_N" "$PORT_V" "$CHANNEL_N" "$LOGGING_YN"
 	fi
 fi
