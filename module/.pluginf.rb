49c30b7b5a10dad9626b230ae455fcde7152f4d0
Alice Duchess <iarcher@pdx.edu>
2015-03-12 07:40:41 -0700

    GOD DAMMIT YOU PIECE OF FUCKING SHIT

--- module/.pluginf.rb
+++ module/.pluginf.rb
@@ -13,24 +13,6 @@ class Pluginf
 		@chan_list.push("any")
 	end
 
-	def
-
-=begin
-	def initialize(regex, name, help, chan)
-		@regexp = Regexp.new(regex.to_s)
-		@name = name.to_s
-		@help = help
-		@chan_list = []
-		@chan_list.push("any")
-		@chan_list.push(chan.to_s)
-	end
-=end
-
-	def cleanup
-		#used if you wish to save some information on exit
-		return ""
-	end
-
 	def script(message, nick, chan)
 		
 	end
@@ -49,6 +31,10 @@ class Pluginf
 
 	def help
 		return @help
+	end
+
+	def cleanup
+		return ""
 	end
 
 end
