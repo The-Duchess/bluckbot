9d87b2a2876caab0e6fbb20f18990b3e60f2c09b
Isaac Archer <archeri@cat.pdx.edu>
2015-03-16 16:03:57 -0700

    adding forcast support

--- module/weather.rb
+++ module/weather.rb
@@ -92,6 +92,94 @@ class Weather < Pluginf
 		return wc.fetch(c.to_i).to_s
 	end
 
+	# returns a length 5 array of ["today", tomorrow", "NEXT NEXT DAY NAME" ... ]
+	def get_day_names
+		# get today
+		arr = ["Today", "Tomorrow", "Next Day", "Next Next Day"]
+
+		# add next days
+
+		return arr
+	end
+
+	# returns the appropriate index for a temperature
+	def get_index(temper)
+		if temper < 40
+			return 0
+		elsif 41 <= temper < 60
+			return 1
+		elsif 61 <= temper < 80
+			return 2
+		else
+			return 3
+		end	
+	end
+
+	def get_forcast(area_code)
+		@ac = area_code
+		@r_w = "\n"
+		days = ["", "", "", "", ""] # weather for various days
+		days_names = get_day_names
+		temp_colors = ["02", "03", "08", "04"] # colors to change text color for temperature
+		
+		url = "api.openweathermap.org/data/2.5/forecast/daily?q=#{@ac}&mode=json&units=imperial&cnt=7"
+		url_m = "api.openweathermap.org/data/2.5/forecast/daily?q=#{@ac}&mode=json&units=metric&cnt=7"
+
+		begin
+			@contents = open(url).read
+		rescue => a
+			return "#{@ac} is this place actually real?"
+		end
+
+		begin
+			@contents_m = open(url_m).read
+		rescue => a
+			return "#{@ac} is this place actually real?"
+		end
+
+		contents = open(url).read
+		contents_m = open(url_m).read
+		parsed_json = JSON.parse(contents)
+		parsed_json_m = JSON.parse(contents_m)
+
+		if parsed_json['main'].nil?
+			@r_w = "#{@ac} is this place actually real?"
+		else
+			# parse website info and put into days
+			days_fc = parsed_json['main']['list']
+			days_fc_m = parsed_json_m['main']
+
+			0.upto(4) do |i|
+				# temperature F
+				temper_f_min = days_fc[i]['temp']['min']
+				t1_n = temp_colors[get_index(temper_f_min)]
+				temper_f_max = days_fc[i]['temp']['max']
+				t1_x = temp_colors[get_index(temper_f_max)]
+				# temperature C
+				temper_c_min = days_fc_m[i]['temp']['min']
+				t2_n = temp_colors[get_index(temper_c_min)]
+				temper_c_max = days_fc_,[i]['temp']['max']
+				t2_x = temp_colors[get_index(temper_c_max)]
+
+				weather_condition = weatherc(days_fc[i]['weather'][0]['id'])
+
+				wind_speed = days_fc[i]['speed']
+
+				humidity = days_fc[i]['humidity']
+
+				days[i].concat("\x0304#{days_names[i]}\x03: Temperature for \x0304#{@ac}\x03: min \x03#{t1_n}#{temper_f_min}\x03°F or \x03#{t2_n}#{temper_c_min}\x03°C, max \x03#{t2_n}#{temper_f_max}\x03°F or \x03#{t2_m}#{temper_c_max}\x03°C, Humidity of #{humidity} percent, Wind speeds at #{wind_speed} mph")
+		end
+
+		days.each do |a|
+			@r_w.concat("#{a}\n")
+		end
+
+		@r_w.chomp!
+
+		return @r_w
+
+	end
+
 	def get_weather(area_code)
 
 		@r_w = ""
@@ -145,7 +233,7 @@ class Weather < Pluginf
 		cmd = tokens[0] # the command the user is calling [ `w <area code |s nick> | `ws <area code> ]
 		@r = "#{nick}: "
 
-		if message.length == 2 # it already matches `w or `ws so it has to be `w
+		if message.length == 2 # it already matches `w or `s
 
 			ac_t = get_ac(nick)
 
@@ -156,6 +244,46 @@ class Weather < Pluginf
 			end
 
 			return @r
+
+		elsif message.match(/^`fc$/) # get your own forcast
+
+			ac_t = get_ac(nick)
+
+			if not ac_t == "nick not found"
+				@r.concat(get_forcast(ac_t).to_s)
+			else
+				@r.concat("nick not found")
+			end
+
+			return @r
+
+
+		elsif cmd == "`fc" # get forcast for a user
+			
+			if (tokens[1] == nick or check_user(tokens[1])) and tokens.length == 2
+				ac_t = get_ac(tokens[1])
+
+				if not ac_t == "nick not found"
+					@r.concat(get_forcast(ac_t).to_s)
+				else
+					@r.concat("nick not found")
+				end
+
+				return @r
+
+			elsif tokens[1] != nick and tokens.length == 3
+				ac_t = ""
+				if tokens.length > 2
+					1.upto(tokens.length - 1) do |i|
+						ac_t.concat("#{i}")
+					end
+				else
+					ac_t = tokens[1]
+				end
+				@r.concat(get_forcast(ac_t).to_s)
+			else
+				return "invalid arguments"
+			end
 
 		elsif cmd == "`w" # getting weather for nick or an area code if tokens[1] != nick
 
@@ -213,7 +341,15 @@ class Weather < Pluginf
 
 				return update_user(nick, ac_t)
 			end
+			ac_t = get_ac(nick)
 
+			if not ac_t == "nick not found"
+				@r.concat(get_weather(ac_t).to_s)
+			else
+				@r.concat("nick not found")
+			end
+
+			return @r
 		else
 			# we have a major problem
 			return "gottverdammt"
@@ -229,13 +365,14 @@ class Weather < Pluginf
 end
 
 prefix_s = [
-		/^`ws/,
-		/^`w/
+		/^`ws /,
+		/^`w/,
+		/^`fc/
 	     ]
 
 reg_p = Regexp.union(prefix_s) #regex to call the module
 na = "weather" #name for plugin #same as file name without .rb
-de = "usage: `w areacode or City, State or nick | `ws <areacode>" #description
+de = "usage: `w areacode or City, State or nick | `ws <nick> <areacode>" #description
 
 #plugin = Class_name.new(regex, name, help)
 #passed back to the plugins_s array
