bdb45496583275503a3505197b6e639b5fb840d1
apels | iruel <The-Duchess@users.noreply.github.com>
2015-03-27 16:38:08 -0700

    Update .template.rb

--- module/.template.rb
+++ module/.template.rb
@@ -1,9 +1,13 @@
 #!/bin/env ruby
-# template as defined by the required file
+#############################################################################################
 # author: apels <Alice Duchess>
 # module as defined by .plugin.rb
-# modules have access to $plugins to be loaded as well as $logs
-# $logs stores unparsed message lines in an array of length 100
+#############################################################################################
+# GLOBALS:
+# - $logs are stored like [most recent - least recent], they are also unparsed
+# - $admins is the list of admins
+# - $plugins_s is the array of active plugins
+#############################################################################################
 
 
 $LOAD_PATH << './module'
@@ -19,11 +23,19 @@ class Eightball < Pluginf
 	end
 end
 
+# allows you to support multiple regexes
+# prefix = [
+#		//,
+#		//
+#	   ]
+#
+# reg_p = Regexp.union(prefix)
+
 reg_p = /^bluckbot:*\?$/ #regex to call the module
 na = "eightball" #name for plugin #same as file name without .rb
 de = "bluckbot: your question? will be answered with an eightball response" #description
 
 #plugin = Class_name.new(regex, name, help)
-#passed back to the plugins_s array
+#pushed onto to the end of plugins array array
 plugin = Eightball.new(reg_p, na, de)
 $plugins_s.push(plugin)
