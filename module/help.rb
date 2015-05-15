84c31c945def7d25b3b1ef6de23a845f0df01430
Isaac Archer <archeri@cat.pdx.edu>
2015-05-15 03:20:30 -0700

    adding reaper themed use info

---
+++ module/help.rb
@@ -0,0 +1,108 @@
+#!/bin/env ruby
+#############################################################################################
+# author: apels <Alice Duchess>
+# module as defined by .plugin.rb
+#############################################################################################
+# GLOBALS:
+# - $logs are stored like [most recent - least recent], they are also unparsed
+# - $admins is the list of admins
+# - $plugins_s is the array of active plugins
+#############################################################################################
+
+
+$LOAD_PATH << './module'
+require '.pluginf.rb'
+
+class Reaperh < Pluginf
+	#any functions you may need
+
+	#your definition for script
+	def script(message, nick, chan)
+
+		# answers about bluckbot
+		reaper_r = [
+					"Rudamentary creatures of blood and flesh, you touch my mind, fumbling in ignorance, incapable of understanding.",
+					"There is a realm of existence so far beyond your own you cannot even imagine it. I am beyond your comprehension.",
+					"We are eternal. The pinnacle of evolution and existence. Before us, you are nothing.",
+					"We have no beginning. We have no end. We are infinite.",
+					"Irc bot, a label given to us by others to give voice to your autism."
+				   ]
+
+		qr = [
+				/what are you/,
+				/bluckbot\?/,
+				/bluckbot is/
+			 ]
+
+		prefix_r = Regexp.union(qr)
+
+		# general questions
+		reaper_a = [
+					"We are eternal, the pinnacle of evolution and existence. Before us, you are nothing.",
+					"We have no beginning. We have no end. We are infinite."
+				   ]
+
+		qa = [
+				/who made you/,
+				/where are you from/,
+				/who created you/,
+				/source\?/
+			 ]
+
+		prefix_a = Regexp.union(qa)
+
+		help_a = [
+				/help/,
+				/Help/,
+			     ]
+
+		prefix_s = Regexp.union(help_a)
+
+		# how can i help
+		reaper_o = ["What do you require fleshling?", "What is it mortal?"]
+
+		tokens = message.split(" ")
+
+		if tokens.length != 2 and message.match(prefix_r)
+			reaper_r.shuffle!
+			return "NOTICE #{nick} :#{reaper_r[2]}"
+		end
+
+		if tokens.length != 2 and message.match(prefix_a)
+			reaper_a.shuffle!
+			return "NOTICE #{nick} :#{reaper_a[1]}"
+
+		end
+
+		if tokens.length < 2 and tokens[1].match(prefix_s)
+			reaper_o.shuffle!
+			return "NOTICE #{nick} :#{reaper_0[1]}"
+		end
+
+		$plugins_s.each do |a|
+			if tokens[1].downcase == a.downcase
+				@r = "NOTICE #{nick} :here mortal\n a.help"
+				return @r
+			end
+		end
+
+		return "NOTICE #{nick} :do not babble at me fleshling, I have little time or patience for your insignificant rambling."
+	end
+end
+
+# allows you to support multiple regexes
+# prefix = [
+#		//,
+#		//
+#	   ]
+#
+# reg_p = Regexp.union(prefix)
+
+reg_p = /^`h / #regex to call the module
+na = "template" #name for plugin #same as file name without .rb
+de = "`h [topic] provides help on modules or if one was not given an appropriate Reaper style response ise given" #description
+
+#plugin = Class_name.new(regex, name, help)
+#pushed onto to the end of plugins array array
+plugin = Reaperh.new(reg_p, na, de)
+$plugins_s.push(plugin)
