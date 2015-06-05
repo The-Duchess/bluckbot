73449b763f385793eefa61b1b778c0116930476a
The-Duchess <The-Duchess@users.noreply.github.com>
2015-06-05 14:46:26 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-TRUE_T="TRUE"
-FALSE_T="FALSE"
 . ./res/.config.sh
-if [ $PASS_YN == $TRUE_T ]; then
+if [[ "$PASS_YN" == "TRUE" ]]; then
 	ruby ./ircboats.rb "$NETWORK_N" "$PORT_V" "$CHANNEL_N" "$LOGGING_YN" "PASS" "$PASSPHRASE"
 else
-	if [ $PASS_YN == $FALSE_T ]; then
+	if [[ "$PASS_YN" == "FALSE" ]]; then
 		ruby ./ircboats.rb "$NETWORK_N" "$PORT_V" "$CHANNEL_N" "$LOGGING_YN"
 	fi
 fi
