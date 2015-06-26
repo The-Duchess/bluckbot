0e4faf5264ec91626f6a81ebf8d13d331e360f49
The-Duchess <iarcher@pdx.edu>
2015-06-25 21:44:41 -0700

    triggerable markov chain module

---
+++ module/markov.rb
@@ -0,0 +1,75 @@
+#!/bin/env ruby
+#############################################################################################
+# author: apels <Alice Duchess>
+# module as defined by .plugin.rb
+#############################################################################################
+# GLOBALS:
+# - $logs are stored like [most recent - least recent], they are also unparsed
+# - $admins is the list of admins
+# - $plugins_s is the array of active plugins
+#############################################################################################
+
+# this plugin requires files from https://github.com/epitron/markovchat to be placed
+# in ./res/
+
+$LOAD_PATH << './module'
+require '.pluginf.rb'
+require 'res/markovchat.rb'
+
+class Template < Pluginf
+	
+	def initialize(regex, name, help)
+		@regexp = Regexp.new(regex.to_s)
+		@name = name.to_s
+		@help = help
+		@chan_list = []
+		@chan_list.push("any")
+
+		@m = nil
+
+		if !File.exists?("markovchain.db")
+			`touch markovchain.db`
+			@m = MarkovChat.new("markovchain.db")
+			#@markov_object.load
+		else
+			@m = MarkovChat.new("markovchain.db")
+			#@markov_object.load
+	    end
+	end
+
+
+	def cleanup
+		m.save
+	end
+
+	#your definition for script
+	def script(message, nick, chan)
+
+		if message.match(/^?/)
+			message = message[1..-1].to_s
+			@r = @m.chat(message)
+			return @r
+		else
+			Process.detach( fork { @m.add_sentence("#{message}") } )
+		end
+
+		return "string"
+	end
+end
+
+allows you to support multiple regexes
+prefix = [
+		//,
+		/^\?/
+	   ]
+
+ reg_p = Regexp.union(prefix)
+
+#reg_p = // #regex to call the module
+na = "template" #name for plugin #same as file name without .rb
+de = "NOTES ^| HELP" #description
+
+#plugin = Class_name.new(regex, name, help)
+#pushed onto to the end of plugins array array
+plugin = Template.new(reg_p, na, de)
+$plugins_s.push(plugin)
