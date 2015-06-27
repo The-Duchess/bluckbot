29950289537638f40f9571294ed233db9804d4b6
Isaac Archer <archeri@cat.pdx.edu>
2015-06-27 04:12:25 -0700

    update

--- ircboats.rb
+++ ircboats.rb
@@ -10,23 +10,25 @@
 # example for ssl     ruby ircboats.rb irc.rizon.net 6697 channelname false
 # example for network with a PASS ruby ircboats.rb somenet 6697 channelname false PASS passphrase
 ####################################################################################################################
-# commands
-# `plsgo : tells the bot to quit
-# `ignore $NICK : tells the bot to ignore a nick
-# `save | `load chans : saves and loads channels from currently active and previously saved
-# /msg bluckbot `list channels : lists channels, must be a pm from the owner
-# `msg $NICK message : sends a message to $NICK
-# `part : parts the active channel
-# `join $#CHANNEL : joins a channel
-# `k $NICK reason: only accessible to the owner and kicks a user from the channel
-# `help : prints help
-# `load $MODULE : loads a module
-# `unload $MODULE : unloads a module
-# `ls : lists modules
-# `list : lists loaded modules
-# `help $MODULE : gives help on a certain module
-# `help modules : gives help for all loaded modules
-# `mass load : loads a preset set of modules in ./res/.modlist
+#>- \`plsgo : tells the bot to quit
+#>- \`ignore $NICK : tells the bot to ignore a nick
+#>- \`unignore $NICK : tells the bot to remove a nick from the ignore list
+#>- \`lsign : gives a list of ignored nicks
+#>- \`save | \`load chans : saves and loads channels from currently active and previously saved
+#>- \`list channels : lists channels
+#>- \`msg $NICK message : sends a message to $NICK
+#>- \`act $CHANNEL action : sends an action to $CHANNEL
+#>- \`part : parts the bot from the channel this is sent from
+#>- \`join $#CHANNEL : joins a channel
+#>- \`k $NICK reason: only accessible to the owner and kicks a user from the channel
+#>- \`help : prints help
+#>- \`load $MODULE : loads a module
+#>- \`unload $MODULE : unloads a module
+#>- \`reload $MODULE : reloads a module
+#>- \`ls : lists modules
+#>- \`list : lists loaded modules
+#>- \`help $MODULE : gives help on a certain module
+#>- \`mass load : loads a preset set of modules in ./res/.modlist
 ####################################################################################################################
 
 require 'socket'
