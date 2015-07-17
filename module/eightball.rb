4ccbcfde8ef171d63f36d7b2cdade4e97773b3ed
Isaac Archer <archeri@cat.pdx.edu>
2015-07-16 18:19:47 -0700

    update

--- module/eightball.rb
+++ module/eightball.rb
@@ -14,26 +14,27 @@ class Eightball < Pluginf
 
 	#your definition for script
 	def script(message, nick, chan)
-		responses = [ "It is certain", 
-"It is decidedly so", 
-"Without a doubt", 
-"Yes definitely", 
-"You may rely on it", 
-"As I see it, yes", 
-"Most likely", 
-"Outlook good", 
-"Yes", 
-"Signs point to yes", 
-"Reply hazy try again", 
-"Ask again later", 
-"Better not tell you now", 
-"Cannot predict now", 
-"Concentrate and ask again", 
-"Don\'t count on it", 
-"My reply is no", 
-"My sources say no", 
-"Outlook not so good", 
-"Very doubtful" ]
+		responses = [ "It is certain",
+				  "It is decidedly so",
+				  "Without a doubt",
+				  "Yes definitely",
+				  "You may rely on it",
+				  "As I see it, yes",
+				  "Most likely",
+				  "Outlook good",
+				  "Yes",
+				  "Signs point to yes",
+				  "Reply hazy try again",
+				  "Ask again later",
+				  "Better not tell you now",
+				  "Cannot predict now",
+				  "Concentrate and ask again",
+				  "Don\'t count on it",
+				  "My reply is no",
+				  "My sources say no",
+				  "Outlook not so good",
+				  "Very doubtful"
+			  	]
 
 		responses.shuffle!
 		return "#{nick}: #{responses[0].to_s}"
